ecaafef09c5fa18a4533b0030bcf62bad0b9f2a2
tristanbnumanglag2024-tech <tristanbnumanglag2024@gmail.com>
2026-09-12 04:05:42 +0800

    Prod api

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,3 +1,3 @@
-export const API_BASE = "https://sakuracareapi.site/rhea-pos-api";
+//export const API_BASE = "https://sakuracareapi.site/rhea-pos-api";
 
-//export const API_BASE = "https://violet-ant-679778.hostingersite.com/prod-pos-api";
+export const API_BASE = "https://violet-ant-679778.hostingersite.com/prod-pos-api";
