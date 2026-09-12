aaf41fff9b75bd340c66937a7675920fc5d92a85
tristanbnumanglag2024-tech <tristanbnumanglag2024@gmail.com>
2026-09-13 00:31:38 +0800

    Configure environment-based API URLs

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,3 +1,4 @@
-export const API_BASE = "https://sakuracareapi.site/rhea-pos-api";
+// export const API_BASE = "https://sakuracareapi.site/rhea-pos-api";
 
 // export const API_BASE = "https://violet-ant-679778.hostingersite.com/prod-pos-api";
+export const API_BASE = import.meta.env.VITE_API_BASE;
